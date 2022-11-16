b4e1cfcd58c78cac7cd2bd203747685b79f02e0e
rangapin <rangapin@gmx.com>
2022-11-16 17:27:26 -0600

    day-one

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php 
+
+$connection = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_crud'
+);
+
+/* 
+if(isset($connection)){
+    echo "DB is connected";
+}
+*/
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("db.php") ?>
+
+<?php include("includes/header.php") ?>
+
+<h1>Hola:)</h1>
+
+<?php include("includes/footer.php") ?>
